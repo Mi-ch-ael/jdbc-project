3ce0df744802857a2b6362bb5b5b236cd6f62c7f
Михаил Эйсвальд <e_misha_vl@mail.ru>
2021-11-26 17:58:01 +0300

    parsing command line arguments

--- src/main/java/testauto/project/service/Main.java
+++ src/main/java/testauto/project/service/Main.java
@@ -1,5 +1,6 @@
 package testauto.project.service;
 
+import java.util.Arrays;
 import java.util.List;
 
 import testauto.project.sqlutil.DAOFactory;
@@ -9,21 +10,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		MySQLDAO dao = DAOFactory.getMySQLDAO();
-		System.out.println(dao);
 		
 		if(dao == null) {
 			System.err.println("Database connection failed. Exiting...");
 			return;
 		}
 		
-		ResultFormatter.format(dao.getMenu(), 
-				"%s: %s per one dish, usually served in %s minutes", "=== Menu ===");
-		
-		ResultFormatter.format(dao.getOrdersAfter(null), 
-				"%s %s ordered %s at %s", "=== Selected orders ===");
-		
-		ResultFormatter.format(dao.getOrdersAfter("2021-09-02 20:00:00"), 
-				"%s %s ordered %s at %s", "=== Selected orders ===");
+		for(int i = 0; i < args.length; ++i) {
+			switch(args[i]) {
+			case "-menu":
+				ResultFormatter.format(dao.getMenu(), 
+						"%s: %s per one dish, usually served in %s minutes", "=== Menu ===");
+				break;
+			case "-orders":
+				if(i+1 < args.length && 
+						args[i+1].matches(
+								"[0-9]{4}-[0-9]{2}-[0-9]{2}@([0-9]{2}:){2}[0-9]{2}"
+								)) {
+					String time = args[i+1].replace('@', ' ');
+					ResultFormatter.format(dao.getOrdersAfter(time), 
+							"%s %s ordered %s at %s", "=== Selected orders ===");
+					i+=1;
+					break;
+				}
+				ResultFormatter.format(dao.getOrdersAfter(null), 
+						"%s %s ordered %s at %s", "=== Selected orders ===");
+				break;
+			default:
+				System.err.format("Warning: unrecognized option \"%s\". Skipped.\n",
+						args[i]);	
+			}
+		}
 		
 		dao.close();
 	}
